0ff58973373e9c8c9e10caa1dc1c82780735e72e
VawnVuxDev <vanvu.nd.97@gmail.com>
2021-11-16 13:02:02 +0900

    Header and Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent ';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -27,8 +30,10 @@ class Main extends Component {
                     </div>
                 </Navbar>
                 <div className="container">
+                    <Header />
                     <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
                     <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                    <Footer />
                 </div>
             </div>
         );
